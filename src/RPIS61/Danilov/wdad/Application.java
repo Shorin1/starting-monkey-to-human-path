083bae440f20882dcce4d0ef6891587a87772d9c
Дмитрий <@DESKTOP-B26T9UQ>
2019-03-29 12:15:06 +0400

    init

---
+++ src/RPIS61/Danilov/wdad/Application.java
@@ -0,0 +1,8 @@
+package RPIS61.Danilov.wdad;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("I'm Dmitry Danilov, and i'm not a monkey");
+    }
+}
